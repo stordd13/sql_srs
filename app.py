a08802d7e3d410eb94d6085ee7e4c709e8715f48
stordd13 <stordeur.bru@gmail.com>
2024-02-12 17:20:21 +0100

    some update on the app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import io
+import ast
 
 import duckdb
 import pandas as pd
@@ -19,7 +20,7 @@ con = duckdb.connect(database="data/exercices_sql_tables.duckdb", read_only=Fals
 with st.sidebar:
     theme = st.selectbox(
         "What would you like to revise ?",
-        ["cross_joins", "Groupby", "Windows Functions"],
+        ["cross_joins", "Groupby", "window_functions"],
         index=None,
         placeholder="Select a theme ...",
     )
@@ -31,30 +32,34 @@ with st.sidebar:
 st.header("enter your code")
 query = st.text_area(label="votre code SQL ici", key="user_input")
 
-# if query:
-#     result = duckdb.sql(query).df()
-#     st.dataframe(result)
+if query: 
+    result = con.execute(query).df()
+    st.dataframe(result)
 
-#     try:
-#         result = result[solution_df.columns]
-#         st.dataframe(result.compare(solution_df))
+    # try:
+    #     result = result[solution_df.columns]
+    #     st.dataframe(result.compare(solution_df))
 
-#     except KeyError as e:
-#         st.write("Some columns are missing")
+    # except KeyError as e:
+    #     st.write("Some columns are missing")
 
-#     n_lines_difference = result.shape[0] - solution_df.shape[0]
-#     if n_lines_difference != 0:
-#         st.write(
-#             f"result has a {n_lines_difference} lines difference with the solution"
-#         )
+    # n_lines_difference = result.shape[0] - solution_df.shape[0]
+    # if n_lines_difference != 0:
+    #     st.write(
+    #         f"result has a {n_lines_difference} lines difference with the solution"
+    #     )
 
 
-# tab2, tab3 = st.tabs(["Tables", "Solution"])
+tab2, tab3 = st.tabs(["Tables", "Solution"])
 
-# with tab2:
+with tab2:
 
-#     st.write("table: beverages")
-#     st.dataframe(beverages)
+    exercice_tables = ast.literal_eval(exercice.loc[0, "tables"])
+    for table in exercice_tables:   
+        st.write(f"table: {table}")
+        df_table = con.execute(f"SELECT * FROM {table}").df()
+        st.dataframe(df_table)
+
 #     st.write("table: food_items")
 #     st.dataframe(food_items)
 #     st.write("expected")

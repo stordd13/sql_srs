4b40e4456409b2f98dce9c94dae186d968a0ef2e
stordd13 <stordeur.bru@gmail.com>
2024-02-13 10:56:47 +0100

    comparison with real answer

--- app.py
+++ app.py
@@ -23,11 +23,16 @@ with st.sidebar:
         ["cross_joins", "Groupby", "window_functions"],
         index=None,
         placeholder="Select a theme ...",
-    )
+    )  
     st.write("You selected :", theme)
-
     exercice = con.execute(f"SELECT * FROM memory_state WHERE theme='{theme}'").df()
     st.write(exercice)
+
+    exercice_name = exercice.loc[0, "exercice_name"]
+    with open(f"answers/{exercice_name}.sql", 'r') as f:
+        answer = f.read()
+    solution_df = con.execute(answer).df()
+
 
 st.header("enter your code")
 query = st.text_area(label="votre code SQL ici", key="user_input")
@@ -36,18 +41,18 @@ if query:
     result = con.execute(query).df()
     st.dataframe(result)
 
-    # try:
-    #     result = result[solution_df.columns]
-    #     st.dataframe(result.compare(solution_df))
+    try:
+        result = result[solution_df.columns]
+        st.dataframe(result.compare(solution_df))
 
-    # except KeyError as e:
-    #     st.write("Some columns are missing")
+    except KeyError as e:
+        st.write("Some columns are missing")
 
-    # n_lines_difference = result.shape[0] - solution_df.shape[0]
-    # if n_lines_difference != 0:
-    #     st.write(
-    #         f"result has a {n_lines_difference} lines difference with the solution"
-    #     )
+    n_lines_difference = result.shape[0] - solution_df.shape[0]
+    if n_lines_difference != 0:
+        st.write(
+            f"result has a {n_lines_difference} lines difference with the solution"
+        )
 
 
 tab2, tab3 = st.tabs(["Tables", "Solution"])
@@ -66,7 +71,5 @@ with tab2:
 #     st.dataframe(solution_df)
 
 with tab3:
-    exercice_name = exercice.loc[0, "exercice_name"]
-    with open(f"answers/{exercice_name}.sql", 'r') as f:
-        answer = f.read()
+
     st.write(answer)
